64c3640e283ad130a0e2f0a18e18a817047e8e00
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-11-15 11:06:20 -0500

    script de mouvements pour les tanks

---
+++ Assets/movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{
+    public InputAction m_movement;
+    public InputAction m_rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Deplacement vers l'avant
+
+        Vector3 direction = transform.forward * m_movement.ReadValue<float>();
+        Vector3 velocity = direction * 15;
+        transform.position += velocity * Time.deltaTime;
+
+        transform.rotation *= new Quaternion(0f, 2f * Time.deltaTime * m_rotation.ReadValue<float>(), 0f, 1f);
+
+    }
+
+    public void OnEnable()
+    {
+        m_movement.Enable();
+        m_rotation.Enable();
+    }
+    public void OnDisable()
+    {
+        m_movement.Disable();
+        m_rotation.Disable();
+    }
+
+}
