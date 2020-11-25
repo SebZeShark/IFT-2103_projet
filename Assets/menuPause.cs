5543ead92fc80fcc21a0e1ed2b49530a09627d55
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-11-25 12:46:18 -0500

    Ajout menu pause

---
+++ Assets/menuPause.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuPause : MonoBehaviour
+{
+    public InputActionAsset settings;
+    InputAction a_pause;
+    public GameObject p_menuPause;
+
+    // Awake is called first
+    void Awake()
+    {
+        a_pause = settings.FindActionMap("UI").FindAction("Pause");
+        p_menuPause.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(a_pause.triggered && !p_menuPause.activeInHierarchy)
+        {
+            Time.timeScale = 0;
+            p_menuPause.SetActive(true);
+        }
+        /*if (a_pause.triggered && p_menuPause.activeInHierarchy)
+        {
+            onRepprendre();
+        }*/
+    }
+
+    public void onRepprendre()
+    {
+        Time.timeScale = 1;
+        p_menuPause.SetActive(false);
+    }
+
+    public void onQuitter()
+    {
+        Time.timeScale = 1;
+        p_menuPause.SetActive(false);
+        SceneManager.LoadScene("Menu");
+    }
+}
